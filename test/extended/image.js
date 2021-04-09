b7baa40166dfc6fe6596934182b384bbca2fbf7b
Somalil <popovandole@gmail.com>
2021-04-09 15:48:56 +0400

    QA8-1155 Added negative cases for Image field, added files, selectors, testData

--- test/extended/image.js
+++ test/extended/image.js
@@ -6,7 +6,7 @@ import exp from '../../data/expected.json';
 
 describe('Image field', function () {
 
-    before('Positive cases', function () {
+    before('Open App', function () {
         browser.url('');
     });
 
@@ -14,36 +14,101 @@ describe('Image field', function () {
         browser.refresh()
     });
 
-    it('TC-105 placeholder = \'Click or drag and drop\'', function () {
-        const placeholderText = $(sel.imagePlaceholder).getText();
-        expect(placeholderText).toEqual(exp.imagePlaceholderText);
+    describe('Positive cases', () => {
+
+        it('TC-105 placeholder = \'Click or drag and drop\'', function () {
+            const placeholderText = $(sel.imagePlaceholder).getText();
+            expect(placeholderText).toEqual(exp.imagePlaceholderText);
+        });
+
+        it('TC-106 Verify that the Image field is optional', function () {
+            input4Values(name.default, sel.radioButtonHe, age.default, story.comedy);
+            $(sel.submit).click();
+            const isStoryDisplayed = $(sel.body).isDisplayed();
+            expect(isStoryDisplayed).toEqual(true);
+        });
+
+        it('TC-108 Validate that user can select jpeg file smaller then 4 mb from file explorer', function () {
+            imageUpload(imagePath.JPG3_9)
+            const isDisplaying = $(sel.imageThumbnail).isExisting()
+            expect(isDisplaying).toEqual(true);
+        });
+
+        it('TC-109 Validate that user can select png smaller then 4 mb file from file explorer', function () {
+            imageUpload(imagePath.PNG3_9)
+            const isDisplaying = $(sel.imageThumbnail).isExisting()
+            expect(isDisplaying).toEqual(true);
+        });
+
+        it('TC-110 Verify that bin icon appears when hover over the image preview', function () {
+            imageUpload(imagePath.JPG1)
+            $(sel.imageIconActions).waitForExist()
+            $(sel.imageIcon).moveTo()
+            const isDisplaying = $(sel.imageRemoveButton).isDisplayed()
+            expect(isDisplaying).toEqual(true);
+        });
+
+        it('TC-111 Validate that user can delete image', function () {
+            imageUpload(imagePath.JPG1)
+            $(sel.imageIconActions).waitForExist()
+            $(sel.imageIcon).moveTo()
+            $(sel.imageRemoveButton).click()
+            const isDeleted = $(sel.image).isDisplayed()
+            expect(isDeleted).toEqual(true);
+        });
+
     });
 
-    it('TC-106 Verify that the Image field is optional', function () {
-        input4Values(name.default, sel.radioButtonHe, age.default, story.comedy);
-        $(sel.submit).click();
-        const isStoryDisplayed = $(sel.body).isDisplayed();
-        expect(isStoryDisplayed).toEqual(true);
-    });
+    describe('Negative cases', () => {
 
-    it('TC-108 Validate that user can select jpeg file smaller then 4 mb from file explorer', function () {
-        imageUpload(imagePath.JPG3_9)
-        const isDisplaying = $(sel.imageThumbnail).isExisting()
-        expect(isDisplaying).toEqual(true);
-    });
+        it('TC-115 Validate that the user is not able to upload jpeg file > 4 mb from file explorer', function () {
+            imageUpload(imagePath.JPG4_1)
+            const err = $(sel.imageError).waitForDisplayed({timeout: 1000})
+            expect(err).toEqual(true);
+        });
 
-    it('TC-109 Validate that user can select png smaller then 4 mb file from file explorer', function () {
-        imageUpload(imagePath.PNG3_9)
-        const isDisplaying = $(sel.imageThumbnail).isExisting()
-        expect(isDisplaying).toEqual(true);
-    });
+        it('TC-116 Validate that  the user is not able to upload png file > 4 mb from file explorer', function () {
+            imageUpload(imagePath.PNG4_1)
+            const err = $(sel.imageError).waitForDisplayed({timeout: 1000})
+            expect(err).toEqual(true);
+        });
 
-    it('TC-110 Verify that bin icon appears when hover over the image preview', function () {
-        imageUpload(imagePath.JPG1)
-        $(sel.imageIconActions).waitForExist()
-        $(sel.imageIcon).moveTo()
-        const isDisplaying = $(sel.imageRemoveButton).isDisplayed()
-        expect(isDisplaying).toEqual(true);
+        it('TC-123 Verify that the user is not able to upload txt file', function () {
+            imageUpload(imagePath.TXT)
+            const err = $(sel.imageError).waitForDisplayed({timeout: 1000})
+            expect(err).toEqual(true);
+        });
+
+        it('TC-124 Verify that the user is not able to upload gif file', function () {
+            imageUpload(imagePath.GIF)
+            const err = $(sel.imageError).waitForDisplayed({timeout: 1000})
+            expect(err).toEqual(true);
+        });
+
+        it('TC-125 Verify that the user is not able to upload bmp file', function () {
+            imageUpload(imagePath.BMP)
+            const err = $(sel.imageError).waitForDisplayed({timeout: 1000})
+            expect(err).toEqual(true);
+        });
+
+        it('TC-126 Verify that the user is not able to upload pdf file', function () {
+            imageUpload(imagePath.PDF)
+            const err = $(sel.imageError).waitForDisplayed({timeout: 1000})
+            expect(err).toEqual(true);
+        });
+
+        it('TC-127 Verify that the user is not able to upload mp4 file', function () {
+            imageUpload(imagePath.MP4)
+            const err = $(sel.imageError).waitForDisplayed({timeout: 1000})
+            expect(err).toEqual(true);
+        });
+
+        it('TC-128 Verify that the user is not able to upload ppt file ', function () {
+            imageUpload(imagePath.PPT)
+            const err = $(sel.imageError).waitForDisplayed({timeout: 1000})
+            expect(err).toEqual(true);
+        });
+
     });
 
 });
